d7ed51670fc18a1e390a9aed6e54b7470a5d91c8
VKati <volo967@gmail.com>
2021-05-09 21:32:33 +0200

    befejeztem az első házifeladatot

--- homework1.js
+++ homework1.js
@@ -17,10 +17,8 @@ for (let i = 0; i < players.length; i++) {
 }
 
 
-// Harmadik feladat - függvény készítése az elemek típusának meghatározásához
-console.log('Harmadik feladat - függvény, amely megvizsgálja az elem típusát és azt egy új tömbe adja vissza');
-
-console.log('Ez egy pár soros megoldása a feladatnak, de nem a megadott feltételek szerint:');
+// Harmadik feladat 
+console.log('Harmadik feladat - megvizsgáljuk a tömb elemének típusát és azt egy új tömbbe adjuk vissza');
 const x = ['', 4, true];
 console.log('Eredeti tömb:', (x));
 let y = [];
